13cdcbbb2a6bd629d76bf0893e69d8a7a1bb76a9
Szymon Kaszura <sajmonpele@gmail.com>
2024-11-05 17:42:21 +0100

    update rag

--- rag.py
+++ rag.py
@@ -57,8 +57,9 @@ config = {"configurable": {"thread_id": "abc123"}}
 system_message = SystemMessage(content= """Use the following pieces of context to answer the question at the end. Please follow the following rules:
 1. If the question is to request links, please only return the source links with no answer.
 2. If you don't know the answer, don't try to make up an answer. Just say **I can't find the final answer but you may want to check the following links** and add the source links as a list.
+3. If you find the answer, write the answer in a concise way and add the list of sources that are used to derive the answer. Exclude the sources that are irrelevant to the final answer.
 """)
-query = "how to restart ec2?"
+query = "how to debug .net application CPU usage in rider"
 for event in agent_executor.stream(
     {"messages": [system_message, HumanMessage(content=query)]},
     config=config,
